87faa686f0c1c42c5744d108a295ee3fab526bc5
William <williammatos09@gmail.com>
2020-03-04 06:54:50 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photos/photo/photo.service'
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
-  photos: Object[] = [];
+  photos: any[] = [];
 
-  constructor(photoService: PhotoService) {
-
-    photoService
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos)
+  constructor(private photoService: PhotoService) {
+    //responsavel por injeção de dependencia.
   }
+  
+  ngOnInit(): void {
+
+    this.photoService
+      .listFromUser('flavio')
+      .subscribe(photos => {
+        this.photos = photos
+      })
+
+    }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,8 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    PhotosModule,
-    HttpClientModule
+    PhotosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
