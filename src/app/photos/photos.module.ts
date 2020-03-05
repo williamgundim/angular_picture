a0c7dd580c649d36bd195d73fab2a2b890aaf065
William <williammatos09@gmail.com>
2020-03-05 06:55:16 -0300

    novo componente photo-list.

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from "@angular/core";
-import { PhotoComponent } from "./photo/photo.component";
 import { HttpClientModule } from "@angular/common/http";
+
+import { PhotoComponent } from "./photo/photo.component";
+import { PhotoListComponent } from './photo-list/photo-list.component';
+import { CommonModule } from "@angular/common";
 
 @NgModule({
 
-    declarations: [ PhotoComponent ],
+    declarations: [ 
+        PhotoComponent, 
+        PhotoListComponent 
+    ],
     exports: [ PhotoComponent ],
-    imports: [ HttpClientModule ]
+    imports: [ 
+        HttpClientModule,
+        CommonModule
+    ]
 
 })
 export class PhotosModule{}
